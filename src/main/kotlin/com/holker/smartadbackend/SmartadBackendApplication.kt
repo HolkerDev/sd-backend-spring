401d9fb1f7dbfdf5166795a10be742d6c47a14ed
HolkerDev <nick.penkovskiy@gmail.com>
2020-03-07 21:03:53 +0100

    Created empty project

---
+++ src/main/kotlin/com/holker/smartadbackend/SmartadBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.holker.smartadbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartadBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartadBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/holker/smartadbackend/SmartadBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.holker.smartadbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartadBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
